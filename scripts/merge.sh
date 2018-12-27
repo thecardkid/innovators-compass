c25d1e5d348d96a0ae90da4e2dd7d206b8fe6ba2
Hieu Nguyen <hieu@shift.com>
2018-12-27 11:56:26 -0800

    Test

--- scripts/merge.sh
+++ scripts/merge.sh
@@ -10,4 +10,27 @@ fi
 git checkout test-branch
 git merge --squash "$curr_branch"
 commit_messages="$(git log --format=%B --reverse develop..${curr_branch})"
+
+cat <<EOF >&2
+
+
+about to squash merge branch "$curr_branch" into develop with the following commit message:
+
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+Merge branch \"$curr_branch\"
+
+$commit_messages
+>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+EOF
+sleep 2
+
+read -p "Continue? [y/N] " input >&2
+if [[ $input != "y" && $input != "Y" ]]; then
+    echo "aborting" >&2
+    exit 1
+fi
+
+echo "committing..." >&2
 git commit -m "Merge branch \"$curr_branch\"" -m "$commit_messages"
+
+git status
