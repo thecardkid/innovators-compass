e2171049d0ee3b7ff9911718ef2d40c5c69d2697
Hieu Nguyen <hieu@shift.com>
2019-01-06 17:33:17 -0800

    Update

--- scripts/wdio-travis.sh
+++ scripts/wdio-travis.sh
@@ -19,11 +19,15 @@ echo "running webdriverio tests..."
 
 set +e
 "$IC_ROOT/node_modules/.bin/wdio" "$IC_ROOT/config/wdio/travis.js"
+set -e
 
 if [[ $? == 1 ]]; then
     pushd "$IC_ROOT/tools/travis-wdio-reporter"
+        echo "generating HTML report.."
         node html-generator.js
-        aws s3 sync "s3://innovatorscompass/wdio-report-${TRAVIS_BUILD_NUMBER}" s3-static/
+        echo "copying report to S3 bucket.."
+        aws s3 sync "s3://innovatorscompass/wdio-report-${TRAVIS_BUILD_NUMBER}" s3-static/ --acl public-read
+        echo -e "\nWebdriverIO failures detected, to see the full report, go to https://s3.us-east-2.amazonaws.com/innovatorscompass/wdio-report-${TRAVIS_BUILD_NUMBER}/index.html"
+        exit 1
     popd
 fi
-set -e
