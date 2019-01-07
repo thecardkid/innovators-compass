ce05cceb94028a188beebc2aa9edf8bd93852748
Hieu Nguyen <hieu@shift.com>
2019-01-06 17:24:06 -0800

    Test flow

--- scripts/wdio-travis.sh
+++ scripts/wdio-travis.sh
@@ -16,4 +16,14 @@ done
 echo "iCompass server detected"
 
 echo "running webdriverio tests..."
+
+set +e
 "$IC_ROOT/node_modules/.bin/wdio" "$IC_ROOT/config/wdio/travis.js"
+
+if [[ $? == 1 ]]; then
+    pushd "$IC_ROOT/tools/travis-wdio-reporter"
+        node html-generator.js
+        aws s3 sync "s3://innovatorscompass/wdio-report-${TRAVIS_BUILD_NUMBER}" s3-static/
+    popd
+fi
+set -e
